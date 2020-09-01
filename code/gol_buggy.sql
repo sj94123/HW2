e2b6f43b3f1a60a66a3c0d3ae582ca15ae0467be
magavade <magavade@ncsu.edu>
2020-09-01 13:45:35 -0400

    Added 3 bugs in SQL: 1. Replaced the 8-neighbor check bug with a syntax bug

--- code/gol_buggy.sql
+++ code/gol_buggy.sql
@@ -18,98 +18,106 @@ VALUES (0, 0, False),
        (2, 1, False),
        (2, 2, False);
 
--- neighbors stores the neighbor count for every cell, default of 0
-CREATE TABLE neighbors
+-- neighbor stores the neighbor count for every cell, default of 0
+CREATE TABLE neighbor
 (
     x int,
     y int,
     n int
 );
 
-CREATE UNIQUE INDEX neighbors_idx on neighbors (x, y);
+CREATE UNIQUE INDEX neighbor_idx on neighbor (x, y);
 
 -- if (x,y) is living, it will be a neighbor to the surrounding 8 cells
 
 -- increment neighbor_count for (x-1,y)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x - 1, y, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x, y-1)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x, y - 1, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x+1, y)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x + 1, y, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x, y+1)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x, y + 1, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x-1, y-1)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x - 1, y - 1, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x+1, y-1)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x + 1, y - 1, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
 -- increment neighbor_count for (x-1, y+1)
-INSERT INTO neighbors
+INSERT INTO neighbor
 SELECT x - 1, y + 1, 1
 FROM grid
 WHERE is_living is True
 ON CONFLICT (x, y)
-    DO UPDATE SET n = neighbors.n + 1;
+    DO UPDATE SET n = neighbor.n + 1;
 
--- delete the neighbors out of the grid boundary
+-- increment neighbor_count for (x+1, y+1)
+INSERT INTO neighbor
+SELECT x + 1, y + 1, 1
+FROM grid
+WHERE is_living is True
+ON CONFLICT (x, y
+    DO UPDATE SET n = neighbor.n + 1;
+
+-- delete the neighbor out of the grid boundary
 DELETE
-FROM neighbors
+FROM neighbor
 WHERE x < 0
   AND y < 0
    OR x > 2
-  AND y > 2;
+   OR y > 2;
 
 -- Status changes:
 
 -- conditions when a cell DIES:
--- 1. if (x, y) has no entry in neighbors -> 0 neighbors
--- 2. if count_neighbors = (1, 4, 5, 6, 7, 8)
+-- 1. if (x, y) has no entry in neighbor -> 0 neighbor
+-- 2. if count_neighbor = (1, 4, 5, 6, 7, 8)
 UPDATE grid g
 SET is_living = False
-WHERE ((g.x, g.y) IN (SELECT x, y FROM neighbors))
-   OR ((SELECT c.n FROM neighbors c WHERE g.x = c.x AND g.y = c.y) IN
+WHERE ((g.x, g.y) IN (SELECT x, y FROM neighbor))
+   OR ((SELECT c.n FROM neighbor c WHERE g.x = c.x AND g.y = c.y) IN
        (1, 4, 5, 6, 7, 8));
 
 -- conditions when a cell LIVES:
--- if (x, y) is DEAD and count_neighbors = 3
+-- if (x, y) is DEAD and count_neighbor = 3
 UPDATE grid g
 SET is_living = True
-WHERE (SELECT c.n FROM neighbors c WHERE g.x = c.x AND g.y = c.y) = 3
+WHERE (SELECT c.n FROM neighbor c WHERE g.x = c.x AND g.y = c.y) = 3
   AND g.is_living = False;
 
 -- stores O/P where ID = xi
